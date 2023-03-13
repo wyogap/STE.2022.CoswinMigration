d526f59f1b2e9f4679a7a39b1d4486ebe7ccc21f
wyogap <wyogap@gmail.com>
2023-03-13 19:03:53 +0700

    - Added STORES to Master Locations - Added ITEM_ and NSITEM to Master Items. Double check for duplicate itemnum - Added Master CompMaster and CompContactMstr - Added Master Companies and CompContact

---
+++ Integration Services Project/script/0003_Master_Locations_002.sql
@@ -0,0 +1,103 @@
+/****** Object:  Table [dbo].[ste_migration_params]    Script Date: 25/01/2023 17:46:34 ******/
+ALTER TABLE LOCATIONS
+ADD STE_MIGRATIONSOURCE VARCHAR(250) default null,
+    STE_MIGRATIONSTOREAUTHORITY VARCHAR(250) DEFAULT NULL;
+;
+
+-- Create post-task
+-- ---------------
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+drop procedure if exists ste_003_master_locations_post
+GO
+
+CREATE PROCEDURE ste_003_master_locations_post
+  @PackageLogID INT
+AS
+BEGIN
+	declare @v_start_id int;
+	declare @v_end_id int;
+	declare @v_cnt int;
+	declare @v_max_id bigint;
+	declare @PackageName varchar(250);
+
+	-- update maximo seq
+	select @v_max_id=max(locationsid) from LOCATIONS;
+	update maxsequence set maxreserved=@v_max_id+1 where tbname='LOCATIONS' and name='LOCATIONSID';
+
+	-- get package name
+	select @PackageName = package_name from [dbo].[ste_migration_logs] where id = @PackageLogID;
+	if (@PackageName is null) return;
+
+	-- update start_id and end_id for FM_FACILITY
+	select @v_start_id=min(STE_MIGRATIONID), @v_end_id=max(STE_MIGRATIONID), @v_cnt=count(STE_MIGRATIONID) from LOCATIONS
+	where STE_MIGRATIONID is not null and STE_MIGRATIONSOURCE='FM_FACILITY';
+
+	insert into [dbo].[ste_migration_log_details] (
+		[package_name]
+		,[log_id]
+		,[event]
+		,[event_type]
+		,[event_description]
+	)
+	values (
+		@PackageName
+		, @PackageLogID
+		, 'FM_FACILITY'
+		, 'COMPLETED'
+		, CONCAT('COUNT: ', @v_cnt, 'START_ID: ', @v_start_id, ', END_ID: ', @v_end_id)
+	);
+
+	-- update start_id and end_id for STORES
+	select @v_start_id=min(STE_MIGRATIONID), @v_end_id=max(STE_MIGRATIONID), @v_cnt=count(STE_MIGRATIONID) from LOCATIONS
+	where STE_MIGRATIONID is not null and STE_MIGRATIONSOURCE='STORES';
+
+	insert into [dbo].[ste_migration_log_details] (
+		[package_name]
+		,[log_id]
+		,[event]
+		,[event_type]
+		,[event_description]
+	)
+	values (
+		@PackageName
+		, @PackageLogID
+		, 'STORES'
+		, 'COMPLETED'
+		, CONCAT('COUNT: ', @v_cnt, 'START_ID: ', @v_start_id, ', END_ID: ', @v_end_id)
+	);
+
+	-- update start_id and end_id
+	select @v_start_id=min(STE_MIGRATIONID), @v_end_id=max(STE_MIGRATIONID) from LOCATIONS
+	where STE_MIGRATIONID is not null;
+
+	UPDATE [dbo].[ste_migration_logs] SET
+	  [start_id] = @v_start_id
+	, [end_id] = @v_end_id
+	WHERE [id] = @PackageLogID
+
+END
+GO
+
+-- update migration params
+-- -----------------------
+INSERT INTO [dbo].[ste_migration_params]
+           ([package_name]
+           ,[parameter_name]
+           ,[parameter_value]
+           ,[created_on]
+           ,[created_by]
+           ,[modified_on]
+           ,[modified_by])
+     VALUES
+           ('0003_Master_Locations'
+           ,'version'
+           ,'3'
+           ,getdate()
+           ,'ssis'
+           ,NULL
+           ,NULL)
+GO
