2acb6daac73d2f43195d074da622f4f617d29bb1
darussalam <sahabatbumi1007@gmail.com>
2023-03-22 22:41:45 +0700

    package 0017

---
+++ Integration Services Project/script/0017_Master_AMCrewType_AMCrew_AMCrewLabor_000.sql
@@ -0,0 +1,278 @@
+/****** Object:  Table [dbo].[ste_migration_params]    Script Date: 25/01/2023 17:46:34 ******/
+
+
+-- ----------- 
+-- AMCREWT
+-- -----------
+
+-- Add custom columns
+-- ------------------
+ALTER TABLE AMCREWT
+ADD STE_MIGRATIONID bigint default null,
+    STE_MIGRATIONDATE datetime NOT NULL DEFAULT (GETDATE());
+;
+
+
+-- Create pre-task
+-- ---------------
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+drop procedure if exists ste_0017_master_amcrewt_pre
+GO
+
+CREATE PROCEDURE ste_0017_master_amcrewt_pre 
+	@PackageLogID INT
+AS
+BEGIN
+	-- truncate existing data
+	delete from amcrewt where STE_MIGRATIONID is not null;
+
+END
+
+GO
+
+-- Create post-task
+-- ---------------
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+drop procedure if exists ste_0017_master_amcrewt_post
+GO
+
+CREATE PROCEDURE ste_0017_master_amcrewt_post
+  @PackageLogID INT
+AS
+BEGIN
+	declare @v_start_id int;
+	declare @v_end_id int;
+	declare @v_cnt int;
+	declare @v_max_id bigint;
+	declare @PackageName varchar(250);
+
+	-- update identity column
+	select @v_max_id=max(amcrewtid) from amcrewt;
+	update maxsequence set maxreserved=@v_max_id+1 where tbname='AMCREWT' and name='AMCREWTID';
+
+	-- get package name
+	select @PackageName = package_name from [dbo].[ste_migration_logs] where id = @PackageLogID;
+	if (@PackageName is null) return;
+
+	-- update start_id and end_id for ITEM_
+	select @v_start_id=min(STE_MIGRATIONID), @v_end_id=max(STE_MIGRATIONID), @v_cnt=count(STE_MIGRATIONID) from amcrewt
+	where STE_MIGRATIONID is not null;
+
+	insert into [dbo].[ste_migration_log_details] (
+		[package_name]
+		,[log_id]
+		,[event]
+		,[event_type]
+		,[event_description]
+	)
+	values (
+		@PackageName
+		, @PackageLogID
+		, 'AMCREWT'
+		, 'COMPLETED'
+		, CONCAT('COUNT: ', @v_cnt, ', START_ID: ', @v_start_id, ', END_ID: ', @v_end_id)
+	);
+
+	-- update start_id and end_id
+	UPDATE [dbo].[ste_migration_logs] SET
+	  [start_id] = @v_start_id
+	, [end_id] = @v_end_id
+	WHERE [id] = @PackageLogID
+
+END
+GO
+
+-- ----------- 
+-- AMCREW
+-- -----------
+
+-- Add custom columns
+-- ------------------
+ALTER TABLE AMCREW
+ADD STE_MIGRATIONID bigint default null,
+    STE_MIGRATIONDATE datetime NOT NULL DEFAULT (GETDATE());
+;
+
+
+-- Create pre-task
+-- ---------------
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+drop procedure if exists ste_0017_master_amcrew_pre
+GO
+
+CREATE PROCEDURE ste_0017_master_amcrew_pre 
+	@PackageLogID INT
+AS
+BEGIN
+	-- truncate existing data
+	delete from amcrew where STE_MIGRATIONID is not null;
+
+END
+
+GO
+
+-- Create post-task
+-- ---------------
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+drop procedure if exists ste_0017_master_amcrew_post
+GO
+
+CREATE PROCEDURE ste_0017_master_amcrew_post
+  @PackageLogID INT
+AS
+BEGIN
+	declare @v_start_id int;
+	declare @v_end_id int;
+	declare @v_cnt int;
+	declare @v_max_id bigint;
+	declare @PackageName varchar(250);
+
+	-- update identity column
+	select @v_max_id=max(amcrewid) from amcrew;
+	update maxsequence set maxreserved=@v_max_id+1 where tbname='AMCREW' and name='AMCREWID';
+
+	-- get package name
+	select @PackageName = package_name from [dbo].[ste_migration_logs] where id = @PackageLogID;
+	if (@PackageName is null) return;
+
+	-- update start_id and end_id for ITEM_
+	select @v_start_id=min(STE_MIGRATIONID), @v_end_id=max(STE_MIGRATIONID), @v_cnt=count(STE_MIGRATIONID) from amcrew
+	where STE_MIGRATIONID is not null;
+
+	insert into [dbo].[ste_migration_log_details] (
+		[package_name]
+		,[log_id]
+		,[event]
+		,[event_type]
+		,[event_description]
+	)
+	values (
+		@PackageName
+		, @PackageLogID
+		, 'AMCREW'
+		, 'COMPLETED'
+		, CONCAT('COUNT: ', @v_cnt, ', START_ID: ', @v_start_id, ', END_ID: ', @v_end_id)
+	);
+
+END
+GO
+
+
+-- ----------- 
+-- AMCREWLABOR
+-- -----------
+
+-- Add custom columns
+-- ------------------
+ALTER TABLE AMCREWLABOR
+ADD STE_MIGRATIONID bigint default null,
+    STE_MIGRATIONDATE datetime NOT NULL DEFAULT (GETDATE());
+;
+
+
+-- Create pre-task
+-- ---------------
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+drop procedure if exists ste_0017_master_amcrewlabor_pre
+GO
+
+CREATE PROCEDURE ste_0017_master_amcrewlabor_pre 
+	@PackageLogID INT
+AS
+BEGIN
+	-- truncate existing data
+	delete from amcrewlabor where STE_MIGRATIONID is not null;
+
+END
+
+GO
+
+-- Create post-task
+-- ---------------
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+drop procedure if exists ste_0017_master_amcrewlabor_post
+GO
+
+CREATE PROCEDURE ste_0017_master_amcrewlabor_post
+  @PackageLogID INT
+AS
+BEGIN
+	declare @v_start_id int;
+	declare @v_end_id int;
+	declare @v_cnt int;
+	declare @v_max_id bigint;
+	declare @PackageName varchar(250);
+
+	-- update identity column
+	select @v_max_id=max(amcrewlaborid) from amcrewlabor;
+	update maxsequence set maxreserved=@v_max_id+1 where tbname='AMCREWlabor' and name='AMCREWLABORID';
+
+	-- get package name
+	select @PackageName = package_name from [dbo].[ste_migration_logs] where id = @PackageLogID;
+	if (@PackageName is null) return;
+
+	-- update start_id and end_id for ITEM_
+	select @v_start_id=min(STE_MIGRATIONID), @v_end_id=max(STE_MIGRATIONID), @v_cnt=count(STE_MIGRATIONID) from amcrewlabor
+	where STE_MIGRATIONID is not null;
+
+	insert into [dbo].[ste_migration_log_details] (
+		[package_name]
+		,[log_id]
+		,[event]
+		,[event_type]
+		,[event_description]
+	)
+	values (
+		@PackageName
+		, @PackageLogID
+		, 'AMCREWLABOR'
+		, 'COMPLETED'
+		, CONCAT('COUNT: ', @v_cnt, ', START_ID: ', @v_start_id, ', END_ID: ', @v_end_id)
+	);
+
+END
+GO
+
+-- update migration params
+-- -----------------------
+INSERT INTO [dbo].[ste_migration_params]
+           ([package_name]
+           ,[parameter_name]
+           ,[parameter_value]
+           ,[created_on]
+           ,[created_by]
+           ,[modified_on]
+           ,[modified_by])
+     VALUES
+           ('0017_Master_AMCrewType_AMCrew_AMCrewLabor'
+           ,'version'
+           ,'1'
+           ,getdate()
+           ,'ssis'
+           ,NULL
+           ,NULL)
+GO
