2a6192c5be4a09d82b7884afb264770093009c3e
darussalam <sahabatbumi1007@gmail.com>
2023-03-22 08:09:52 +0700

    package master 0014, 0015, 0016

---
+++ Integration Services Project/script/0016_Master_Labor_000.sql
@@ -0,0 +1,110 @@
+/****** Object:  Table [dbo].[ste_migration_params]    Script Date: 25/01/2023 17:46:34 ******/
+-- ----------- 
+-- LABOR
+-- -----------
+
+-- Add custom columns
+-- ------------------
+ALTER TABLE LABOR
+ADD STE_MIGRATIONID bigint default null,
+    STE_MIGRATIONDATE datetime NOT NULL DEFAULT (GETDATE());
+;
+
+
+-- Create pre-task
+-- ---------------
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+drop procedure if exists ste_0016_master_labor_pre
+GO
+
+CREATE PROCEDURE ste_0016_master_labor_pre 
+	@PackageLogID INT
+AS
+BEGIN
+	-- truncate existing data
+	delete from labor where STE_MIGRATIONID is not null;
+
+END
+
+GO
+
+-- Create post-task
+-- ---------------
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+drop procedure if exists ste_0016_master_labor_post
+GO
+
+CREATE PROCEDURE ste_0016_master_labor_post
+  @PackageLogID INT
+AS
+BEGIN
+	declare @v_start_id int;
+	declare @v_end_id int;
+	declare @v_cnt int;
+	declare @v_max_id bigint;
+	declare @PackageName varchar(250);
+
+	-- update identity column
+	select @v_max_id=max(laborid) from labor;
+	update maxsequence set maxreserved=@v_max_id+1 where tbname='LABOR' and name='LABORID';
+
+	-- get package name
+	select @PackageName = package_name from [dbo].[ste_migration_logs] where id = @PackageLogID;
+	if (@PackageName is null) return;
+
+	-- update start_id and end_id for ITEM_
+	select @v_start_id=min(STE_MIGRATIONID), @v_end_id=max(STE_MIGRATIONID), @v_cnt=count(STE_MIGRATIONID) from labor
+	where STE_MIGRATIONID is not null;
+
+	insert into [dbo].[ste_migration_log_details] (
+		[package_name]
+		,[log_id]
+		,[event]
+		,[event_type]
+		,[event_description]
+	)
+	values (
+		@PackageName
+		, @PackageLogID
+		, 'LABOR'
+		, 'COMPLETED'
+		, CONCAT('COUNT: ', @v_cnt, ', START_ID: ', @v_start_id, ', END_ID: ', @v_end_id)
+	);
+
+	-- update start_id and end_id
+	UPDATE [dbo].[ste_migration_logs] SET
+	  [start_id] = @v_start_id
+	, [end_id] = @v_end_id
+	WHERE [id] = @PackageLogID
+
+END
+GO
+
+
+-- update migration params
+-- -----------------------
+INSERT INTO [dbo].[ste_migration_params]
+           ([package_name]
+           ,[parameter_name]
+           ,[parameter_value]
+           ,[created_on]
+           ,[created_by]
+           ,[modified_on]
+           ,[modified_by])
+     VALUES
+           ('0016_Master_Labor'
+           ,'version'
+           ,'1'
+           ,getdate()
+           ,'ssis'
+           ,NULL
+           ,NULL)
+GO
