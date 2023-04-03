f3af979ad64cb00c6b9b03b287010f9e6d64cf44
wyogap <wyogap@gmail.com>
2023-04-04 06:31:15 +0700

    - Added: 0309_PO_ContractPurch

---
+++ Integration Services Project/script/0309_PO_ContractPurch_000.sql
@@ -0,0 +1,127 @@
+/****** Object:  Table [dbo].[ste_migration_params]    Script Date: 25/01/2023 17:46:34 ******/
+-- Add custom columns
+-- ------------------
+IF COLUMNPROPERTY(OBJECT_ID('dbo.contractpurch'), 'STE_MIGRATIONID', 'ColumnId') is null
+ALTER TABLE contractpurch
+ADD STE_MIGRATIONSOURCE varchar(16) null,
+	STE_MIGRATIONID bigint default null,
+    STE_MIGRATIONDATE datetime NOT NULL DEFAULT (GETDATE());
+
+
+-- Create pre-task
+-- ---------------
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+drop procedure if exists ste_0309_po_contractpurch_pre
+GO
+
+CREATE PROCEDURE ste_0309_po_contractpurch_pre 
+	@PackageLogID INT
+AS
+BEGIN
+	-- truncate existing data
+	delete from dbo.contractpurch where STE_MIGRATIONID is not null;
+
+END
+
+GO
+
+-- Create post-task
+-- ---------------
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+drop procedure if exists ste_0309_po_contractpurch_post
+GO
+
+CREATE PROCEDURE ste_0309_po_contractpurch_post
+  @PackageLogID INT
+AS
+BEGIN
+	declare @v_start_id int;
+	declare @v_end_id int;
+	declare @v_cnt int;
+	declare @v_max_id bigint;
+	declare @PackageName varchar(250);
+
+	-- update identity column
+	select @v_max_id=max(contractpurchid) from contractpurch;
+	update maxsequence set maxreserved=@v_max_id+1 where tbname='CONTRACTPURCH' and name='CONTRACTPURCHID';
+
+	-- get package name
+	select @PackageName = package_name from [dbo].[ste_migration_logs] where id = @PackageLogID;
+	if (@PackageName is null) return;
+
+	-- update start_id and end_id for ITEM_
+	select @v_start_id=min(STE_MIGRATIONID), @v_end_id=max(STE_MIGRATIONID), @v_cnt=count(STE_MIGRATIONID) from contractpurch
+	where STE_MIGRATIONID is not null AND STE_MIGRATIONSOURCE='SUPP_AGREE';
+
+	insert into [dbo].[ste_migration_log_details] (
+		[package_name]
+		,[log_id]
+		,[event]
+		,[event_type]
+		,[event_description]
+	)
+	values (
+		@PackageName
+		, @PackageLogID
+		, 'CONTRACT (PRICE)'
+		, 'COMPLETED'
+		, CONCAT('COUNT: ', @v_cnt, ', START_ID: ', @v_start_id, ', END_ID: ', @v_end_id)
+	);
+
+	select @v_start_id=min(STE_MIGRATIONID), @v_end_id=max(STE_MIGRATIONID), @v_cnt=count(STE_MIGRATIONID) from contractpurch
+	where STE_MIGRATIONID is not null AND STE_MIGRATIONSOURCE='PO';
+
+	insert into [dbo].[ste_migration_log_details] (
+		[package_name]
+		,[log_id]
+		,[event]
+		,[event_type]
+		,[event_description]
+	)
+	values (
+		@PackageName
+		, @PackageLogID
+		, 'CONTRACT (BLANKET)'
+		, 'COMPLETED'
+		, CONCAT('COUNT: ', @v_cnt, ', START_ID: ', @v_start_id, ', END_ID: ', @v_end_id)
+	);
+
+	select @v_start_id=min(STE_MIGRATIONID), @v_end_id=max(STE_MIGRATIONID), @v_cnt=count(STE_MIGRATIONID) from contractpurch
+	where STE_MIGRATIONID is not null;
+
+	-- update start_id and end_id
+	UPDATE [dbo].[ste_migration_logs] SET
+	  [start_id] = @v_start_id
+	, [end_id] = @v_end_id
+	WHERE [id] = @PackageLogID
+
+END
+GO
+
+-- update migration params
+-- -----------------------
+INSERT INTO [dbo].[ste_migration_params]
+           ([package_name]
+           ,[parameter_name]
+           ,[parameter_value]
+           ,[created_on]
+           ,[created_by]
+           ,[modified_on]
+           ,[modified_by])
+     VALUES
+           ('0309_PO_ContractPurch'
+           ,'version'
+           ,'1'
+           ,getdate()
+           ,'ssis'
+           ,NULL
+           ,NULL)
+GO
