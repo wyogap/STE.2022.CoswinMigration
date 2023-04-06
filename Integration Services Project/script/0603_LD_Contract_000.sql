a63bc7d7b779d6c1a36c1ed1bc40f1aa85b4519b
wyogap <wyogap@gmail.com>
2023-04-06 18:06:28 +0700

    - Added: 0601_LD_CompMaster - Added: 0602_LD_Asset - Added: 0603_LD_Contract - Added: 0604_LD_PR - Added: 0605_LD_Workorder - Added: 0606_LD_SR

---
+++ Integration Services Project/script/0603_LD_Contract_000.sql
@@ -0,0 +1,109 @@
+/****** Object:  Table [dbo].[ste_migration_params]    Script Date: 25/01/2023 17:46:34 ******/
+-- Add custom columns
+-- ------------------
+IF COLUMNPROPERTY(OBJECT_ID('dbo.longdescription'), 'STE_MIGRATIONID', 'ColumnId') is null
+ALTER TABLE longdescription
+ADD STE_MIGRATIONID bigint default null,
+    STE_MIGRATIONDATE datetime NOT NULL DEFAULT (GETDATE());
+
+-- Create pre-task
+-- ---------------
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+drop procedure if exists ste_0603_LD_Contract_pre
+GO
+
+CREATE PROCEDURE ste_0603_LD_Contract_pre 
+	@PackageLogID INT
+AS
+BEGIN
+	-- truncate existing data
+	delete from longdescription where STE_MIGRATIONID is not null and ldownertable='CONTRACT';
+
+	-- reset hasld flag
+	update contract
+	set
+		hasld=0
+	where STE_MIGRATIONID is not null;
+
+END
+
+GO
+
+-- Create post-task
+-- ---------------
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+drop procedure if exists ste_0603_LD_Contract_post
+GO
+
+CREATE PROCEDURE ste_0603_LD_Contract_post
+  @PackageLogID INT
+AS
+BEGIN
+	declare @v_start_id int;
+	declare @v_end_id int;
+	declare @v_cnt int;
+	declare @v_max_id bigint;
+	declare @PackageName varchar(250);
+
+	-- update identity column
+	select @v_max_id=max(longdescriptionid) from longdescription;
+	update maxsequence set maxreserved=coalesce(@v_max_id,0)+1 where tbname='LONGDESCRIPTION' and name='LONGDESCRIPTIONID';
+
+	-- get package name
+	select @PackageName = package_name from [dbo].[ste_migration_logs] where id = @PackageLogID;
+	if (@PackageName is null) return;
+
+	-- update start_id and end_id for ITEM_
+	select @v_start_id=min(STE_MIGRATIONID), @v_end_id=max(STE_MIGRATIONID), @v_cnt=count(STE_MIGRATIONID) from longdescription
+	where STE_MIGRATIONID is not null and ldownertable='CONTRACT';
+
+	insert into [dbo].[ste_migration_log_details] (
+		[package_name]
+		,[log_id]
+		,[event]
+		,[event_type]
+		,[event_description]
+	)
+	values (
+		@PackageName
+		, @PackageLogID
+		, 'MATCH'
+		, 'COMPLETED'
+		, CONCAT('COUNT: ', @v_cnt, ', START_ID: ', @v_start_id, ', END_ID: ', @v_end_id)
+	);
+
+	UPDATE [dbo].[ste_migration_logs] SET
+	  [start_id] = @v_start_id
+	, [end_id] = @v_end_id
+	WHERE [id] = @PackageLogID
+
+END
+GO
+
+-- update migration params
+-- -----------------------
+INSERT INTO [dbo].[ste_migration_params]
+           ([package_name]
+           ,[parameter_name]
+           ,[parameter_value]
+           ,[created_on]
+           ,[created_by]
+           ,[modified_on]
+           ,[modified_by])
+     VALUES
+           ('0603_LD_Contract'
+           ,'version'
+           ,'1'
+           ,getdate()
+           ,'ssis'
+           ,NULL
+           ,NULL)
+GO
