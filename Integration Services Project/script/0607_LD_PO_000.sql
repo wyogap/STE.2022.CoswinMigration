14570905bd6ebe2d02d938c50840f38fdddc4771
wyogap <wyogap@gmail.com>
2023-04-07 11:44:42 +0700

    - Added: 0607_LD_PO

---
+++ Integration Services Project/script/0607_LD_PO_000.sql
@@ -0,0 +1,134 @@
+/****** Object:  Table [dbo].[ste_migration_params]    Script Date: 25/01/2023 17:46:34 ******/
+-- Add custom columns
+-- ------------------
+IF COLUMNPROPERTY(OBJECT_ID('dbo.longdescription'), 'STE_MIGRATIONID', 'ColumnId') is null
+ALTER TABLE longdescription
+ADD STE_MIGRATIONID bigint default null,
+    STE_MIGRATIONDATE datetime NOT NULL DEFAULT (GETDATE());
+
+IF COLUMNPROPERTY(OBJECT_ID('dbo.longdescription'), 'STE_MIGRATIONSOURCE', 'ColumnId') is null
+ALTER TABLE longdescription
+ADD STE_MIGRATIONSOURCE VARCHAR(20) default null;
+
+-- Create pre-task
+-- ---------------
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+drop procedure if exists ste_0607_LD_PO_pre
+GO
+
+CREATE PROCEDURE ste_0607_LD_PO_pre 
+	@PackageLogID INT
+AS
+BEGIN
+	-- truncate existing data
+	delete from longdescription where STE_MIGRATIONID is not null and ldownertable='PO';
+
+	-- reset hasld flag
+	update PO
+	set
+		hasld=0
+	where STE_MIGRATIONID is not null;
+
+END
+
+GO
+
+-- Create post-task
+-- ---------------
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+drop procedure if exists ste_0607_LD_PO_post
+GO
+
+CREATE PROCEDURE ste_0607_LD_PO_post
+  @PackageLogID INT
+AS
+BEGIN
+	declare @v_start_id int;
+	declare @v_end_id int;
+	declare @v_cnt int;
+	declare @v_max_id bigint;
+	declare @PackageName varchar(250);
+
+	-- update identity column
+	select @v_max_id=max(longdescriptionid) from longdescription;
+	update maxsequence set maxreserved=coalesce(@v_max_id,0)+1 where tbname='LONGDESCRIPTION' and name='LONGDESCRIPTIONID';
+
+	-- get package name
+	select @PackageName = package_name from [dbo].[ste_migration_logs] where id = @PackageLogID;
+	if (@PackageName is null) return;
+
+	-- update start_id and end_id for ITEM_
+	select @v_start_id=min(STE_MIGRATIONID), @v_end_id=max(STE_MIGRATIONID), @v_cnt=count(STE_MIGRATIONID) from longdescription
+	where STE_MIGRATIONID is not null and ldownertable='PO' AND STE_MIGRATIONSOURCE='REMARK';
+
+	insert into [dbo].[ste_migration_log_details] (
+		[package_name]
+		,[log_id]
+		,[event]
+		,[event_type]
+		,[event_description]
+	)
+	values (
+		@PackageName
+		, @PackageLogID
+		, 'MATCH-REMARK'
+		, 'COMPLETED'
+		, CONCAT('COUNT: ', @v_cnt, ', START_ID: ', @v_start_id, ', END_ID: ', @v_end_id)
+	);
+
+	select @v_start_id=min(STE_MIGRATIONID), @v_end_id=max(STE_MIGRATIONID), @v_cnt=count(STE_MIGRATIONID) from longdescription
+	where STE_MIGRATIONID is not null and ldownertable='PO' AND STE_MIGRATIONSOURCE='FOLLOWUP';
+
+	insert into [dbo].[ste_migration_log_details] (
+		[package_name]
+		,[log_id]
+		,[event]
+		,[event_type]
+		,[event_description]
+	)
+	values (
+		@PackageName
+		, @PackageLogID
+		, 'MATCH-FOLLOWUP'
+		, 'COMPLETED'
+		, CONCAT('COUNT: ', @v_cnt, ', START_ID: ', @v_start_id, ', END_ID: ', @v_end_id)
+	);
+
+	select @v_start_id=min(STE_MIGRATIONID), @v_end_id=max(STE_MIGRATIONID), @v_cnt=count(STE_MIGRATIONID) from longdescription
+	where STE_MIGRATIONID is not null and ldownertable='PO';
+
+	UPDATE [dbo].[ste_migration_logs] SET
+	  [start_id] = @v_start_id
+	, [end_id] = @v_end_id
+	WHERE [id] = @PackageLogID
+
+END
+GO
+
+-- update migration params
+-- -----------------------
+INSERT INTO [dbo].[ste_migration_params]
+           ([package_name]
+           ,[parameter_name]
+           ,[parameter_value]
+           ,[created_on]
+           ,[created_by]
+           ,[modified_on]
+           ,[modified_by])
+     VALUES
+           ('0607_LD_PO'
+           ,'version'
+           ,'1'
+           ,getdate()
+           ,'ssis'
+           ,NULL
+           ,NULL)
+GO
